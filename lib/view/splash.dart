2efc4f9f8b7540f2037bbc66fb94a31a24810704
RaniaMostafa55 <raniamostafah55@gmail.com>
2023-06-30 19:06:32 +0300

    context_problem

--- lib/view/splash.dart
+++ lib/view/splash.dart
@@ -1,5 +1,4 @@
 import 'package:renta_app/controller/imports.dart';
-import 'package:renta_app/view/login_loading_page.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
@@ -10,9 +9,11 @@ class SplashView extends StatelessWidget {
       splash: 'assets/images/logo.png',
       nextScreen: CacheHelper.getBool(key: "isFirstTime", choice: true)
           ? const OnBoarding()
-          : (CacheHelper.getBool(key: "isLoggedIn", choice: false) == true)
-              ? const LoginLoading()
-              : const LoginView(),
+          :
+          // (CacheHelper.getBool(key: "isLoggedIn", choice: false) == true)
+          //     ? const LoginLoading()
+          //     :
+          const LoginView(),
       // const OnBoarding(),
       splashTransition: SplashTransition.sizeTransition,
       curve: Curves.easeIn,
